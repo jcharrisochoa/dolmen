c9d38b8d949518513e32090cb115e215dd341ba9
Johan Charris <jcharrisochoa@gmail.com>
2020-10-07 10:59:37 -0500

    correccion error de camara api 29 version 10

---
+++ app/src/main/java/co/dolmen/sid/ListaActividadActivity.java
@@ -0,0 +1,13 @@
+package co.dolmen.sid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaActividadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_actividad);
+    }
+}
